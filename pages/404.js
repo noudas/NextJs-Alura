00d4ac4ae0cd7b5e8e8a07e538522a393dcd5301
noudas <noudas1@gmail.com>
2025-07-16 13:20:04 +0300

    Create page 404

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Linki from "../src/components/Link";
+
+export default function FaqPage(){
+    return(
+        <div>
+            <h1>Você se perdeu e caiu na página 404 :0</h1>
+            <Linki href="/">
+            Voltar para Home
+            </Linki>
+        </div>
+    )
+}
