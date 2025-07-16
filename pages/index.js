4a88fcfa7d12e14a6f6ae52a555d897d2f3aa061
noudas <noudas1@gmail.com>
2025-07-16 13:27:32 +0300

    Title component on main, testing style jsx

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Linki from "../src/components/Link";
 
 
+
+function Title({children, as}){
+  const Tag = as;
+  return(
+    <>
+    <Tag>
+      {children}
+    </Tag>
+    <style jsx>
+      {`
+      ${Tag} {
+      color: red;
+          }
+      `}
+    </style>
+    </>
+
+  )
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home</h1>
+      <Title as="h1">Alura Cases - Home</Title>
         <Linki href={'./faq'}>
           FAQ
         </Linki>
