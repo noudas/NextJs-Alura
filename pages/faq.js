a7a0f965557a788f605fb3c51a1660e006449aa1
noudas <noudas1@gmail.com>
2025-07-17 14:30:23 +0300

    Created Faq page using both getStaticProps and getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,16 @@
 // import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
+// getServerSideProps - Carrega em tempo de execução, toda vez que uma requisição é feita ele recarrega e é executado no servidor
+// export async function getServerSideProps(){
+//     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+//     const faq = await fetch(url)
+//         .then((response) => {
+//             return response.json()
+//         })
+// }
+
+// getStaticProps - Carrega em tempo de build, é executado no servidor somente uma vez.
 export async function getStaticProps(){
     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     
