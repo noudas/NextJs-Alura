ecf83be40802049ef27c36bcdd6dbc281c9c31b6
noudas <noudas1@gmail.com>
2025-07-17 14:16:45 +0300

    used dinamic props to creat static data

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,55 @@
+// import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export default function FAQPage() {
+export async function getStaticProps(){
+    const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    
+    const faq= await fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            return data;
+        })
+
+    return {
+        props:{
+            faq
+        }
+    };
+}
+
+export default function FAQPage({ faq }) {
+    // const [faq, setFaq] = useState([]);
+    // useEffect(() => {
+    //     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    //     fetch(url)
+    //         .then((response) => {
+    //             return response.json()
+    //         })
+    //         .then((data) => {
+    //             setTimeout(()=>{
+    //                 setFaq(data);
+    //             },3 * 1000)
+    //         })
+    // })
     return (
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
             </Link>
+
+            <ul>
+                {faq.map(({answer, question}) => {
+                    return (
+                        <li key={question}>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
