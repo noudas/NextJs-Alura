295b8d2e4e27dc12b0b372172229e7d07a2f2cf5
noudas <noudas1@gmail.com>
2025-07-17 15:06:52 +0300

    Unpacking more class

--- pages/faq.js
+++ pages/faq.js
@@ -1,64 +1,53 @@
-// import { useEffect, useState } from 'react';
+import Head from 'next/head'
 import Link from '../src/components/Link';
 
-// getServerSideProps - Carrega em tempo de execução, toda vez que uma requisição é feita ele recarrega e é executado no servidor
-// export async function getServerSideProps(){
-//     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-//     const faq = await fetch(url)
-//         .then((response) => {
-//             return response.json()
-//         })
-// }
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
 
-// getStaticProps - Carrega em tempo de build, é executado no servidor somente uma vez.
-export async function getStaticProps(){
-    const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-    
-    const faq= await fetch(url)
-        .then((response) => {
-            return response.json()
+// export async function getServerSideProps() {
+//     console.log('Em modo DEV, sempre roda! A cada acesso')
+//     console.log('Rodando a cada acesso que você recebe')
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
         })
-        .then((data) => {
-            return data;
-        })
+        .then((resposta) => {
+            return resposta;
+        });
 
     return {
-        props:{
-            faq
-        }
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
     };
-}
+}  
 
 export default function FAQPage({ faq }) {
-    // const [faq, setFaq] = useState([]);
-    // useEffect(() => {
-    //     const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-    //     fetch(url)
-    //         .then((response) => {
-    //             return response.json()
-    //         })
-    //         .then((data) => {
-    //             setTimeout(()=>{
-    //                 setFaq(data);
-    //             },3 * 1000)
-    //         })
-    // })
+    console.log('Isso roda no servidor???')
     return (
         <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
             </Link>
-
             <ul>
-                {faq.map(({answer, question}) => {
-                    return (
-                        <li key={question}>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
                             <h2>{question}</h2>
                             <p>{answer}</p>
-                        </li>
-                    )
-                })}
+                        </article>
+                    </li>
+                ))}
             </ul>
         </div>
     )
